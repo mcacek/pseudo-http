9935cd08e4d546e1d4422e198321d3a8f3c8a695
Michael Cacek <michael.cacek@objectpartners.com>
2018-06-18 23:31:59 -0500

    Refactor code to decouple modules

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-require('dotenv').config();
+'use strict';
 
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const profiles = require('./lib/profiles');
 const routes = require('./lib/routes');
 
-module.exports = function (config, profileName) {
-	const profile = profiles.load(profileName);
-
+module.exports.start = function (config, profile) {
 	console.log(`Serving profile "${profile.label}" at http://localhost:${config.port}`);
 	app.use(cors({origin: true, credentials: true}));
 	routes.registerProfile(app, profile);
