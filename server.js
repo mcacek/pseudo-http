f7c0eb4125a6dd44eb5601bd04c9275bb074230e
Michael Cacek <michael.cacek@gmail.com>
2020-02-06 10:00:42 -0600

    Update dependencies
    Update dependencies and replace xo with prettier.

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
-const routes = require('./lib/routes');
+const routes = require("./lib/routes");
 
-module.exports.start = function (config, profile) {
-	console.log(`Serving profile "${profile.label}" at http://localhost:${config.port}`);
-	app.use(cors({origin: true, credentials: true}));
-	routes.registerProfile(app, profile);
+module.exports.start = function(config, profile) {
+  console.log(
+    `Serving profile "${profile.label}" at http://localhost:${config.port}`
+  );
+  app.use(cors({ origin: true, credentials: true }));
+  routes.registerProfile(app, profile);
 
-	app.listen(config.port);
+  app.listen(config.port);
 };
